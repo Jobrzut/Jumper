9223656f5867311b6244632efcc3ba2e69d81393
Julek <90630830+Jobrzut@users.noreply.github.com>
2023-01-23 20:40:56 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+var postac = document.getElementById("postac")
+var przeszkoda = document.getElementById("przeszkoda")
+var sekundy = 0;
+var licznik = document.getElementById('liczba')
+var wynik = document.getElementById('h2suck')
+
+function skok() {
+    
+    if(postac.classList != "animate") {
+        postac.classList.add("animate");
+    }
+
+    setTimeout(function(){
+        postac.classList.remove("animate");
+    },500);
+
+}
+
+var sprawdzSmierc = setInterval(function() {
+    var gorapostaci = parseInt(window.getComputedStyle(postac).getPropertyValue("top"));
+    var lewaprzeszkody = parseInt(window.getComputedStyle(przeszkoda).getPropertyValue("left"));
+
+    if (lewaprzeszkody < 100 && lewaprzeszkody > 0 && gorapostaci >= 350) {
+        kontener.style.display = "none";
+        suck.style.display = "block";
+        wynik.innerText = "Score: " + sekundy;
+    }
+},10);
+
+
+setInterval(function dodawanieSekund() {
+    sekundy = sekundy + 1;
+    licznik.innerText = sekundy;
+    console.log(sekundy);
+}, 1000);
